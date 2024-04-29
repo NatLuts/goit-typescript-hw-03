61057590d935a9890fb3df1774ade8637ce2578f
NatLuts <lutsenko.natusiia@gmail.com>
2024-04-29 10:45:00 +0300

    upd

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,39 @@
+class Key {
+  constructor(private signature: number = Math.random()) {}
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  private tenants: Person[] = [];
+  public door = false;
+
+  constructor(public key: Key) {}
+  comeIn(tenant: Person): void {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+
+  abstract openDoor(key: Key);
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (this.key.getSignature === key.getSignature) {
+      this.door = true;
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +42,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
